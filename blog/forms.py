ec514303d9d647eb179ee95b10192e5f0433cc50
Jacob-Kasunzu <jacobkasunzu1@gmail.com>
2022-06-03 08:40:53 +0300

    first commit

---
+++ blog/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import PostModel,Comment
+
+class PostModelForm(forms.ModelForm):
+    content=forms.CharField(widget=forms.Textarea(attrs={'rows': 4}))
+    class Meta:
+        model = PostModel
+        fields = ('title', 'content')
+
+class PostUpdateForm(forms.ModelForm):
+    class Meta:
+        model = PostModel
+        fields = ('title', 'content')
+        
+class CommentForm(forms.ModelForm):
+    content=forms.CharField(label='',widget=forms.TextInput(attrs={'placeholder': 'Add a comment here.....'}))
+    class Meta:
+        model = Comment
+        fields = ('content',)
+
+        
---
+++ users/views.py
@@ -0,0 +1,45 @@
+from multiprocessing import context
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+
+
+def sign_up(request):
+    if request.method =="POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users-login')
+            
+    else:
+        form = SignUpForm()
+    
+    
+    context={
+        'form':form,
+    }
+    return render(request, 'users/sign_up.html', context)
+
+
+# Create your views here.
+@login_required
+def profile(request):
+    if request.method =='POST':
+        u_form = UserUpdateForm(request.POST or None, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST or None, request.FILES or None, instance=request.user.profilemodel)
+        
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('users-profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profilemodel)
+        
+        
+    context={
+        'u_form':u_form,
+        'p_form':p_form,
+    }
+    return render(request, 'users/profile.html', context)
